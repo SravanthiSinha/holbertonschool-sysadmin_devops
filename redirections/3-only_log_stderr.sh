570d9a2eae9634b4017e00c07fd8252d61e57dd3
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-03-07 22:15:46 -0800

    fixing the scripts

--- redirections/3-only_log_stderr.sh
+++ redirections/3-only_log_stderr.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 mv &> /tmp/3-only_log_stderr
+cat /tmp/3-only_log_stderr
--- redirections/4-stdout_and_stderr.sh
+++ redirections/4-stdout_and_stderr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo $2
-$1 &> /tmp/4-stdout_and_stderr
+echo $2 > /tmp/4-stdout_and_stderr 2>&1
+$1 >> /tmp/4-stdout_and_stderr 2>&1
 cat /tmp/4-stdout_and_stderr
